6ed883d1d22bdb8a35778b983285a0550e6affe4
Eerick6 <erick199335@gmail.com>
2025-01-21 15:17:15 -0500

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/digital_waiter')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/digital_waiter/browser/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
