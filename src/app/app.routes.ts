cb9ab4bdc19f8359220864c611575f3e6c866e03
Eerick6 <erick199335@gmail.com>
2025-01-21 20:00:48 -0500

    Funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/waiter/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 
